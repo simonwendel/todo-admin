366a4ac029d491e3fadf6d23d02cd53427cb39e2
Simon Wendel <mail@simonwendel.se>
2017-03-08 14:00:48 +0100

    feat: Add EF context for the Authentication table

---
+++ server/src/TodoAdmin.Persistence/TodoStorageContext.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoAdmin.Persistence
+{
+    using Microsoft.EntityFrameworkCore;
+
+    internal partial class TodoStorageContext : DbContext
+    {
+        public virtual DbSet<AuthenticationEntity> Authentication { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            optionsBuilder.UseSqlServer(@"Server=localhost\sqlexpress;Database=TodoStorage_Stage;User Id=sa;Password=sa;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AuthenticationEntity>(entity =>
+            {
+                entity
+                    .HasKey(e => e.AppId)
+                    .HasName("PK_Authentication_AppId");
+
+                entity
+                    .Property(e => e.AppId)
+                    .ValueGeneratedNever();
+
+                entity
+                    .Property(e => e.AccountName)
+                    .IsRequired()
+                    .HasMaxLength(255);
+
+                entity
+                    .Property(e => e.Created)
+                    .HasColumnType("datetime2")
+                    .HasDefaultValueSql("getdate()");
+
+                entity
+                    .Property(e => e.Secret)
+                    .HasMaxLength(32)
+                    .HasColumnType("binary(32)");
+            });
+        }
+    }
+}
---
+++ server/test/TodoAdmin.Persistence.Tests/TodoStorageContextTests.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Todo Storage for wifeys Todo app.
+ * Copyright (C) 2016  Simon Wendel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace TodoAdmin.Persistence.Tests
+{
+    using System;
+    using FluentAssertions;
+    using Xunit;
+
+    public class TodoStorageContextTests
+    {
+        /// <summary>
+        /// Not terribly elegant integration test making sure complete roundtripping 
+        /// of <see cref="AuthenticationEntity" /> object works as expected.
+        /// </summary>
+        [Fact]
+        public void Context_ShouldRoundTripEntity()
+        {
+            var entity = new AuthenticationEntity
+            {
+                AppId = Guid.NewGuid(),
+                AccountName = "SomeAccount",
+                Created = new DateTime(2017, 1, 2, 1, 32, 45),
+                Secret = new byte[] { 1, 2, 3 }
+            };
+
+            // save
+            using (var sut = new TodoStorageContext())
+            {
+                sut.Add(entity);
+                sut.SaveChanges();
+            }
+
+            // query
+            using (var sut = new TodoStorageContext())
+            {
+                sut.Authentication
+                    .Should().ContainSingle(e => e.Equals(entity));
+            }
+
+            // delete
+            using (var sut = new TodoStorageContext())
+            {
+                sut.Authentication.Remove(entity);
+                sut.SaveChanges();
+            }
+
+            // query
+            using (var sut = new TodoStorageContext())
+            {
+                sut.Authentication
+                    .Should().NotContain(e => e.Equals(entity));
+            }
+        }
+    }
+}
