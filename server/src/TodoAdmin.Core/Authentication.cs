60d10493ae7553a995622bfeb899fde8306275e2
Simon Wendel <mail@simonwendel.se>
2017-03-31 23:35:52 +0200

    fix: Expose Authentication setters to allow model binding

--- server/src/TodoAdmin.Core/Authentication.cs
+++ server/src/TodoAdmin.Core/Authentication.cs
@@ -27,16 +27,16 @@ namespace TodoAdmin.Core
     {
         private const int SecretLength = 32;
 
-        public Guid AppId { get; internal set; }
+        public Guid AppId { get; set; }
 
         [MaxLength(255)]
-        public string AccountName { get; internal set; }
+        public string AccountName { get; set; }
 
         [MinLength(SecretLength)]
         [MaxLength(SecretLength)]
-        public byte[] Secret { get; internal set; }
+        public byte[] Secret { get; set; }
 
-        public DateTime Created { get; internal set; }
+        public DateTime Created { get; set; }
 
         public static Authentication New()
         {
