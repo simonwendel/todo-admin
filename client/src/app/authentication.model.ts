0d846f875174e15da3f5752b055c6c44c70ecf3c
Simon Wendel <mail@simonwendel.se>
2017-04-14 14:13:18 +0200

    chore: Fix whitespace around if in authentication.model

--- client/src/app/authentication.model.ts
+++ client/src/app/authentication.model.ts
@@ -23,7 +23,7 @@ export class Authentication {
     public secret: string;
 
     constructor(fields: Authentication = undefined) {
-        if(fields) {
+        if (fields) {
             Object.assign(this, fields);
         }
     }
