0280d35c13c2eda52a30fbb899e5585e15a5c9f9
Simon Wendel <mail@simonwendel.se>
2017-04-16 08:17:15 +0200

    chore: Re-order imports in app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -22,8 +22,8 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 
-import {AuthenticationService} from './shared';
 import {AppComponent} from './app.component';
+import {AuthenticationService} from './shared';
 import {AuthenticationListComponent} from './authentication-list';
 
 @NgModule({
