3e60ebcc0721c0fbd158624ed63a83d10524a8be
Simon Wendel <mail@simonwendel.se>
2017-04-23 21:47:56 +0200

    refactor: Remove unused stuff from auth dialog component

--- client/src/app/authentication-list/authentication-dialog.component.ts
+++ client/src/app/authentication-list/authentication-dialog.component.ts
@@ -17,18 +17,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'tc-authentication-dialog',
     templateUrl: './authentication-dialog.html',
     styleUrls: ['./authentication-dialog.css']
 })
-export class AuthenticationDialogComponent implements OnInit {
-
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
+export class AuthenticationDialogComponent {
 }
