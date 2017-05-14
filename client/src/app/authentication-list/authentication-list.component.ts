ec6a8f66f7028e32e1cc8677d57ae00787b146cd
Simon Wendel <mail@simonwendel.se>
2017-05-14 17:48:42 +0200

    chore: Remove unused import in auth list component

--- client/src/app/authentication-list/authentication-list.component.ts
+++ client/src/app/authentication-list/authentication-list.component.ts
@@ -17,7 +17,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import {Component, OnInit, Input} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import {Authentication} from '../shared';
 import {AuthenticationService} from '../shared/authentication.service';
