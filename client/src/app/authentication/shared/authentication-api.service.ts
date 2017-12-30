b237cbad285dfb4d331bae1dc0ed7ba667334a8d
Simon Wendel <mail@simonwendel.se>
2017-12-30 18:11:33 +0100

    feat: Add AppConfig property to auth api service

--- client/src/app/authentication/shared/authentication-api.service.ts
+++ client/src/app/authentication/shared/authentication-api.service.ts
@@ -19,6 +19,11 @@
 
 import {Injectable} from '@angular/core';
 
+import {AppConfig} from '../../app.config';
+
 @Injectable()
 export class AuthenticationApiService {
+
+    constructor(private readonly config: AppConfig) {
+    }
 }
